0664d4cfe834cf85fca2537bcceb40cd780dd963
Chupligin Sergey <neochapay@gmail.com>
2022-01-14 09:26:50 +0000

    Rework order

--- deploy.sh
+++ deploy.sh
@@ -22,26 +22,12 @@ else
     BUILD="${BUILD-buildpkg -n -p}"
 fi
 
-# apps
-eval "$BUILD google-opensans-fonts"
-eval "$BUILD nemo-theme-glacier-git"
-eval "$BUILD nemo-theme-openmoko-git"
-eval "$BUILD maliit-nemo-keyboard-git"
-eval "$BUILD qt5-quickcontrols-nemo-git"
-eval "$BUILD qt5-glacier-app-git"
-eval "$BUILD glacier-calc-git"
+# NemoMobile packages
 eval "$BUILD nemo-qml-plugin-settings-git"
-eval "$BUILD glacier-camera-git"
 eval "$BUILD nemo-qml-plugin-folderlistmodel"
-eval "$BUILD glacier-filemuncher-git"
 eval "$BUILD libresource"
 eval "$BUILD libresourceqt"
 eval "$BUILD qtmpris"
-eval "$BUILD glacier-music-git"
-eval "$BUILD glacier-packagemanager-git"
-eval "$BUILD glacier-polkit-agent"
-
-# homescreen/de
 eval "$BUILD mce-headers"
 eval "$BUILD libiphb"
 eval "$BUILD nemo-keepalive"
@@ -50,10 +36,8 @@ eval "$BUILD glacier-devicelock-plugin"
 eval "$BUILD mlite"
 eval "$BUILD nemo-qml-plugin-thumbnailer"
 eval "$BUILD qtdocgallery-git"
-eval "$BUILD glacier-gallery-git"
 eval "$BUILD nemo-qml-plugin-configuration"
 eval "$BUILD nemo-qml-plugin-statusnotifier-git"
-eval "$BUILD qt5-quickcontrols-nemo-examples-git"
 eval "$BUILD libiodata"
 eval "$BUILD sailfish-access-control"
 eval "$BUILD tzdata-timed"
@@ -71,7 +55,6 @@ eval "$BUILD libmce-qt"
 eval "$BUILD qtcontacts-sqlite"
 eval "$BUILD nemo-qml-plugin-notifications"
 eval "$BUILD libqofono-qt5"
-eval "$BUILD glacier-pinquery-git" # additional app
 eval "$BUILD libqofonoext"
 eval "$BUILD libconnman-qt"
 eval "$BUILD libngf-qt"
@@ -79,22 +62,15 @@ eval "$BUILD libusb-moded-qt"
 eval "$BUILD profiled-settings-nemo"
 eval "$BUILD profiled"
 eval "$BUILD nemo-qml-plugin-systemsettings-git"
-eval "$BUILD qt5-lipstick-git"
 eval "$BUILD qt5-systems"
 eval "$BUILD nemo-qml-plugin-connectivity"
-eval "$BUILD glacier-settings-git"
-
 eval "$BUILD buteo-syncfw"
 eval "$BUILD buteo-sync-plugin-carddav"
 eval "$BUILD nemo-qml-plugin-contacts"
-
 eval "$BUILD libprofile-qt"
 eval "$BUILD qt-mobility-haptics-ffmemless"
-
-#multimedia stuff
 eval "$BUILD pulsecore-headers"
 eval "$BUILD pulseaudio-module-keepalive"
-
 eval "$BUILD libdsme"
 eval "$BUILD dsme"
 eval "$BUILD swi-prolog7"
@@ -106,18 +82,10 @@ eval "$BUILD libdres-ohm"
 eval "$BUILD policy-settings-common"
 eval "$BUILD ohm-plugins-misc"
 eval "$BUILD ohm-rule-engine"
-
 eval "$BUILD fingerterm"
-
-eval "$BUILD glacier-testtool-git"
 eval "$BUILD ngfd"
 eval "$BUILD ngfd-settings-nemo"
 eval "$BUILD ngfd-plugin-native-vibrator"
-
-
-eval "$BUILD lipstick-glacier-home-git"
-eval "$BUILD glacier-wayland-session-git"
-
 eval "$BUILD libcommhistory"
 eval "$BUILD commhistory-daemon"
 eval "$BUILD qmf-qt5"
@@ -125,20 +93,42 @@ eval "$BUILD nemo-qml-plugin-accounts"
 eval "$BUILD nemo-qml-plugin-messages"
 eval "$BUILD mkcal"
 eval "$BUILD contactsd"
-eval "$BUILD glacier-contacts-git"
 eval "$BUILD telepathy-mission-control"
-eval "$BUILD glacier-messages-git"
 eval "$BUILD voicecall"
-eval "$BUILD glacier-dialer-git"
-
 eval "$BUILD nemo-qml-plugin-email"
-eval "$BUILD glacier-mail-git"
-
-eval "$BUILD glacier-browser-git"
-
-# need to be clarified
 eval "$BUILD usb-tethering"
+eval "$BUILD qt5-feedback-haptics-native-vibrator"
+eval "$BUILD libsocialcache"
+eval "$BUILD buteo-sync-plugins-social"
+eval "$BUILD nemo-qml-plugin-alarms-git"
 
+#Glacier UX packages
+eval "$BUILD google-opensans-fonts"
+eval "$BUILD nemo-theme-glacier-git"
+eval "$BUILD nemo-theme-openmoko-git"
+eval "$BUILD maliit-nemo-keyboard-git"
+eval "$BUILD qt5-quickcontrols-nemo-git"
+eval "$BUILD qt5-glacier-app-git"
+eval "$BUILD glacier-calc-git"
+eval "$BUILD glacier-filemuncher-git"
+eval "$BUILD glacier-camera-git"
+eval "$BUILD glacier-music-git"
+eval "$BUILD glacier-packagemanager-git"
+eval "$BUILD glacier-polkit-agent"
+eval "$BUILD glacier-gallery-git"
+eval "$BUILD qt5-quickcontrols-nemo-examples-git"
+eval "$BUILD glacier-pinquery-git"
+eval "$BUILD qt5-lipstick-git"
+eval "$BUILD glacier-settings-git"
+eval "$BUILD glacier-testtool-git"
+eval "$BUILD lipstick-glacier-home-git"
+eval "$BUILD glacier-wayland-session-git"
+eval "$BUILD glacier-contacts-git"
+eval "$BUILD glacier-messages-git"
+eval "$BUILD glacier-dialer-git"
+eval "$BUILD glacier-mail-git"
+eval "$BUILD glacier-browser-git"
+eval "$BUILD glacier-weather-git"
 
 # pure-maps packages
 if [ "$BUILDARCH" != "aarch64" ]; then
@@ -150,16 +140,6 @@ eval "$BUILD s2geometry-git"
 eval "$BUILD mapbox-gl-native-git"
 eval "$BUILD mapbox-gl-qml-git"
 eval "$BUILD pure-maps-git"
-
-eval "$BUILD glacier-weather-git"
-
-
-eval "$BUILD qt5-feedback-haptics-native-vibrator"
-eval "$BUILD libsocialcache"
-eval "$BUILD buteo-sync-plugins-social"
-
-eval "$BUILD nemo-qml-plugin-alarms-git"
-
 
 # arm specific packages
 if [ "$BUILDARCH" == "aarch64" ]; then
