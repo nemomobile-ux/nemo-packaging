936a1d0690029332022191dced3d7775e8d5b624
Jozef Mlich <jmlich83@gmail.com>
2022-12-29 10:20:24 +0100

    make cache for list of packages, to allow change all dependencies properly

--- list_of_pkgs.sh
+++ list_of_pkgs.sh
@@ -3,6 +3,12 @@
 # prints list of packages as declare -p PACKAGES_BUILD which can be loaded using eval
 #
 
+if [ -f "list_of_pkgs.cache" ]; then
+    source "list_of_pkgs.cache"
+    declare -p PACKAGES_BUILD
+    exit 0
+fi
+
 declare -a PACKAGES_BUILD
 
 function check_add() {
@@ -18,3 +24,4 @@ BUILDARCH=x86_64
 source ./deploy.sh &> /dev/null
 
 declare -p PACKAGES_BUILD
+declare -p PACKAGES_BUILD > list_of_pkgs.cache
